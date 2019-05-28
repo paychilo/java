bef5e901c1e14e50decfa4de06afb01b031e88f7
Ivan <paychilo@gmail.com>
2019-05-28 22:47:19 +0300

    Картинки при клике

--- diploma/src/js/parts/pict.js
+++ diploma/src/js/parts/pict.js
@@ -1,12 +1,13 @@
 function pict() {
   let pict = document.querySelector('.works'),
     href = pict.getElementsByTagName('a'),
-    overlay = document.getElementsByClassName('overlay')[0],
+    overlay = document.querySelector('.overlay'),
     div = document.createElement('div');
-  
-    href[0].addEventListener('click', function (event) {
+
+  for (let i = 0; i < href.length; i++) {
+      href[i].addEventListener('click', function (event) {
       event.preventDefault();
-      div.innerHTML = `<img src="img/our_works/big_img/1.png" alt="">`;
+      div.innerHTML = `<img src="img/our_works/big_img/${i+1}.png" alt="">`;
       div.classList.add('img_pict');
       pict.appendChild(div);
       overlay.style.display = 'block';
@@ -18,21 +19,8 @@ function pict() {
         }
       });
     });
-  
-    href[1].addEventListener('click', function (event) {
-      event.preventDefault();
-      div.innerHTML = `<img src="img/our_works/big_img/2.png" alt="">`;
-      div.classList.add('img_pict');
-      pict.appendChild(div);
-      overlay.style.display = 'block';
-      div.addEventListener('click', function (event) {
-        let target = event.target;
-        if (target.classList.contains('img_pict')) {
-          overlay.style.display = "none";
-          div.remove();
-        }
-      });
-    });
+  }
+    
 }
 
 module.exports = pict;
